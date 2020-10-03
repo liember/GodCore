189476741a799be6911c5d880f61c86e6842a388
liember <liember@yandex.ru>
2020-10-04 03:56:13 +0700

    just main file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace uwp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Entities.Units.Folower("liember");
+            Console.WriteLine(f.Health);
+        }
+    }
+}
